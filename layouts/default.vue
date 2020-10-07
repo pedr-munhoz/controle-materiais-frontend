7da7d8c2c3fdf995c71e7b2cb6fb5f6a69dc02f0
Pedro Munhoz <pedr.munhoz@gmail.com>
2020-10-07 15:55:13 -0300

    Botão legado retirado do layout default.

--- layouts/default.vue
+++ layouts/default.vue
@@ -28,9 +28,6 @@
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
